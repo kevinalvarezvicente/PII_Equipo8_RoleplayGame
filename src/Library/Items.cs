c0f27d2a60ab6b25445ab530d7d9204ddfa11036
martino-lucas95 <martino.lucas95@gmail.com>
2021-09-17 10:18:50 -0300

    Creación de clases Elves e Items.

---
+++ src/Library/Items.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace Library
+{
+    public class Sword
+    {
+        private int damage {get; set;}
+        public int Damage{get;}
+
+        public string Name{get; set;}
+        public Sword (int damage, string Name)
+        {
+            this.Damage = damage;
+            this.Name = Name;
+        }
+
+        public int GetDamage()
+        {
+            return this.Damage;
+        }
+        public string GetName()
+        {
+            return this.Name;
+        }
+    }
+} 
