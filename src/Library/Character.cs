8185b824082a781e8c546103b01bdf4d4bdd6079
Pablo Méndez <valentino.mendez@correo.ucu.edu.uy>
2021-10-04 01:32:35 -0300

    update character"

---
+++ src/Library/Character.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Library
+{
+    class Character
+    {
+
+        private string Name { get; set; }
+        public int Health { get; set; }
+        public int AttackValue { get; set; }     
+        public int DefenseValue { get; set; }
+
+        List<Items> ListItems = new List<Items>();
+        public void AddItem(Items item)
+        {
+            if (ListItems.Count <= 3)
+            {
+                ListItems.Add(item);
+                Console.WriteLine($"The item {item.Name} was added.");
+            }
+            else
+            {
+                Console.WriteLine($"You can not have more than 4 items so remove one.");
+                Console.WriteLine($"The item called {item.Name} was not added.");
+                showItems();
+            }
+        }
+
+        public void RemoveItem(Items item)
+        {
+            ListItems.Remove(item);
+            Console.WriteLine($"The item {item.Name} was removed.");
+            showItems();
+        }
+
+        public void showItems()
+        {
+            Console.WriteLine($"Your items are: ");
+            for (int i = 0; i < ListItems.Count; i++)
+            {
+                Console.WriteLine($"The item {ListItems[i].Name}");
+            }
+        }
+
+        /*
+        public int getAttack()
+        {
+            Console.WriteLine($"The Damage of the Wizard is {this.AttackValue}");
+            return this.AttackValue;
+        }
+        */
+
+        public void Cure()
+        {
+             Console.WriteLine("You want to use a potion? y/n");
+             
+             string r = Console.ReadLine();
+             if (r == "y" || r == "Y")
+             {
+                this.Health = this.Health + 50;
+             }
+             else if (r == "n" || r == "N")
+             {
+                
+             }
+             else
+             {
+                 Console.WriteLine("You have touch a wrong key");
+             }
+        }
+
+        public void ReceiveAttack(int power)
+        {
+            
+        }
+
+    }
+}
